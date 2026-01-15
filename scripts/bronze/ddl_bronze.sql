2a8bdb0fa624d0e55304a85008657b2ecd004d3b
Aravindgithub11 <126331962+Aravindgithub11@users.noreply.github.com>
2026-01-15 17:00:51 +0100

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,70 @@
+	CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
+	BEGIN
+
+	PRINT'========================================================='
+	PRINT'Loading Bronze layer'
+	PRINT'========================================================='
+
+	PRINT'---------------------------------------------------------'
+	PRINT'Loading CRM Tables'
+
+	TRUNCATE TABLE bronze.crm_cust_info;
+		BULK INSERT bronze.crm_cust_info
+		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+		FIRSTROW=2,
+		FIELDTERMINATOR =',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE bronze.crm_prd_info
+		BULK INSERT bronze.crm_prd_info
+		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+		FIRSTROW=2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE bronze.crm_sales_details
+		BULK INSERT bronze.crm_sales_details
+		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH(
+		FIRSTROW=2,
+		FIELDTERMINATOR=',',
+		TABLOCK
+	);
+
+	
+	PRINT'---------------------------------------------------------'
+	PRINT'Loading ERP Tables'
+
+
+	TRUNCATE TABLE bronze.erp_CUST_AZ12
+		BULK INSERT bronze.erp_CUST_AZ12
+		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH(
+		FIRSTROW=2,
+		FIELDTERMINATOR=',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE bronze.erp_LOC_A101
+		BULK INSERT bronze.erp_LOC_A101
+		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH(
+		FIRSTROW=2,
+		FIELDTERMINATOR=',',
+		TABLOCK
+	);
+
+
+	TRUNCATE TABLE bronze.erp_PX_CAT_G1V2
+		BULK INSERT bronze.erp_PX_CAT_G1V2
+		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+		FIRSTROW=2,
+		FIELDTERMINATOR=',',
+		TABLOCK
+	);
+	END
