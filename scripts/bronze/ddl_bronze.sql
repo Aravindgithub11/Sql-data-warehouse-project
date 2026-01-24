871fa0b8e470cd9a3b06ff6c1ffcb6c625cc7624
Aravindgithub11 <126331962+Aravindgithub11@users.noreply.github.com>
2026-01-24 03:28:16 +0100

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,70 +1,58 @@
-	CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
-	BEGIN
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL 
+DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_material_status NVARCHAR(50),
+cst_gndr NVARCHAR (50),
+cst_create_date DATE
+);
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL 
+DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL 
+DROP TABLE bronze.crm_sales_details;
 
-	PRINT'========================================================='
-	PRINT'Loading Bronze layer'
-	PRINT'========================================================='
+CREATE TABLE bronze.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sös_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+IF OBJECT_ID ('bronze.erp_LOC_A101', 'U') IS NOT NULL 
+DROP TABLE bronze.erp_LOC_A101;
+CREATE TABLE bronze.erp_LOC_A101(
+cid NVARCHAR(50),
+cntry NVARCHAR(50),
+);
+IF OBJECT_ID ('bronze.erp_PX_CAT_G1V2', 'U') IS NOT NULL 
+DROP TABLE bronze.erp_PX_CAT_G1V2;
+CREATE TABLE bronze.erp_PX_CAT_G1V2(
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
 
-	PRINT'---------------------------------------------------------'
-	PRINT'Loading CRM Tables'
+IF OBJECT_ID ('bronze.erp_CUST_AZ12', 'U') IS NOT NULL 
+DROP TABLE bronze.erp_CUST_AZ12;
+CREATE TABLE bronze.erp_CUST_AZ12(
+cid NVARCHAR(50),
+bdate DATE,
+gen NVARCHAR(50));
 
-	TRUNCATE TABLE bronze.crm_cust_info;
-		BULK INSERT bronze.crm_cust_info
-		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-		WITH (
-		FIRSTROW=2,
-		FIELDTERMINATOR =',',
-		TABLOCK
-	);
-
-	TRUNCATE TABLE bronze.crm_prd_info
-		BULK INSERT bronze.crm_prd_info
-		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-		WITH (
-		FIRSTROW=2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-
-	TRUNCATE TABLE bronze.crm_sales_details
-		BULK INSERT bronze.crm_sales_details
-		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-		WITH(
-		FIRSTROW=2,
-		FIELDTERMINATOR=',',
-		TABLOCK
-	);
-
-	
-	PRINT'---------------------------------------------------------'
-	PRINT'Loading ERP Tables'
-
-
-	TRUNCATE TABLE bronze.erp_CUST_AZ12
-		BULK INSERT bronze.erp_CUST_AZ12
-		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-		WITH(
-		FIRSTROW=2,
-		FIELDTERMINATOR=',',
-		TABLOCK
-	);
-
-	TRUNCATE TABLE bronze.erp_LOC_A101
-		BULK INSERT bronze.erp_LOC_A101
-		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-		WITH(
-		FIRSTROW=2,
-		FIELDTERMINATOR=',',
-		TABLOCK
-	);
-
-
-	TRUNCATE TABLE bronze.erp_PX_CAT_G1V2
-		BULK INSERT bronze.erp_PX_CAT_G1V2
-		FROM 'D:\SQL_Data_Warehouse_Project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-		WITH (
-		FIRSTROW=2,
-		FIELDTERMINATOR=',',
-		TABLOCK
-	);
-	END
